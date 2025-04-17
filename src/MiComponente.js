c20d23618b28c1d936a769ae2893944787aba1cc
JJMEJIAAVILA <jhon_jmejia@soy.sena.edu.co>
2025-04-17 02:15:53 -0500

    feat: Añade un botón y manejo de evento onClick al componente MiComponente

--- src/MiComponente.js
+++ src/MiComponente.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './MiComponente.css';
+
 function MiComponente() {
+    const handleClick = () => {
+        alert('¡Se hizo clic en el botón!');
+    };
+
     return (
         <div className="mi-componente">
             <h2>Este es mi primer componente</h2>
             <p>¡Estoy aprendiendo React!</p>
+            <button onClick={handleClick}>Haz clic aquí</button>
         </div>
     );
 }
