2847a845d6309f1a6d832bc4fe54fc61dfdcd66a
JJMEJIAAVILA <jhon_jmejia@soy.sena.edu.co>
2025-04-17 01:24:40 -0500

    feat: Renderiza el componente MiComponente en App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
+import React from 'react';
+import MiComponente from './MiComponente';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Mi Aplicación React</h1> {/* título como en el tutorial */}
+          <MiComponente /> {/* Aquí se renderiza el componente */}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
